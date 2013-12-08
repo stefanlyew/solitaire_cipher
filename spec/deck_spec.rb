aeb85e32c3e0a8551a1731c10e8429813b520d1a
Stefan Lyew <stefan.lyew@gmail.com>
2013-12-08 18:25:22 -0500

    Added Deck, deck can move a single card

---
+++ spec/deck_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require 'solitaire_cipher/deck'
+
+describe SolitaireCipher::Deck do
+  let(:deck) { SolitaireCipher::Deck.new }
+  let(:cards) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "A", "B"] }
+
+  it "initializes a deck" do
+    expect(deck.deck).to eq cards
+  end
+
+  describe "#move" do
+    it "cycles the card at the index one position to the right" do
+      expect(deck.move(2)).to eq [1, 3, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "A", "B"]
+    end
+    
+    it "cycles the last card back to first position" do
+      expect(deck.move('B')).to eq ["B", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "A"]
+    end
+  end
+
+end
