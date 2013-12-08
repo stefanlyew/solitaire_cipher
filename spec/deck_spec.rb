306415a77bc52a7cd0411aae66dfb7b29d0491a9
Stefan Lyew <stefan.lyew@gmail.com>
2013-12-08 18:48:54 -0500

    triple cut complete

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -19,4 +19,15 @@ describe SolitaireCipher::Deck do
     end
   end
 
+  describe "#triple_cut" do
+    #TODO Maybe we should Test this more extensively for unhappy cases
+    #take all the cards above top joker, and all cards below bottom joker
+    #switch the places of the cut sections
+    it "cuts the deck on the position of the two jokers" do
+      deck.deck = [1,"B", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, "A", 52]
+
+    expect(deck.triple_cut).to eq [52, "B", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, "A", 1]
+    end
+  end
+
 end
