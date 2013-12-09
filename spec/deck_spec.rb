9949c60ad0e9f93dc552fc793863e84efbb5aaf5
Stefan Lyew <stefan.lyew@gmail.com>
2013-12-08 20:59:15 -0500

    count cut

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -30,4 +30,12 @@ describe SolitaireCipher::Deck do
     end
   end
 
+  describe "#count_cut" do
+    it "takes the value of the bottom card and cuts that number of cards off the top of the deck inserting them above the bottom card" do
+      deck.deck = ["B", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "A", 1]
+
+      expect(deck.count_cut).to eq [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, "A", "B", 1]
+    end
+  end
+
 end
